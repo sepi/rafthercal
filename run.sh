68c34ecc6da9479b30c49d39db82683252c57841
Raffael Mancini <raffael.mancini@kussbus.lu>
2021-12-18 18:13:15 +0100

    Added google tasks

--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@ cd /home/pi/rafthercal
 # Weather in BXL
 ./weather.sh
 # Todo list
-python todo.py | lp -o raw
+python google_tasks.py | lp -o raw
 # Calendar
-python cal.py | lp -o raw
+python google_cal.py | lp -o raw
 # Fortune cookie
 fortune -s | cowsay -W 29 | lp -o raw
 echo | lp
