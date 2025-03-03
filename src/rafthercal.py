d87aebf1bc7cd524cc2bf3fad33821b2f6a1dec0
Raffael Mancini <raffael.mancini@mnhn.lu>
2025-03-03 23:11:15 +0100

    Nicer main module

--- src/rafthercal.py
+++ src/rafthercal.py
@@ -1,44 +1,45 @@
 import sys
-import os
-import platform
 from  datetime import date, datetime
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from rml.rml import print_from_str
 
 from loader import load_plugin_classes
-
 import config
 
-base_context = {
-    'today': date.today(),
-    'now': datetime.now().time(),
-    'line': "----------------------------",
-}
+def load_context():
+    base_context = {
+        'today': date.today(),
+        'now': datetime.now().time(),
+        'line': "----------------------------",
+    }
 
-plugin_classes = load_plugin_classes()
-plugins = [klass(config) for _, klass in plugin_classes.items()]
+    plugin_classes = load_plugin_classes()
+    plugins = [klass(config) for _, klass in plugin_classes.items()]
 
-# Collect contexts from all plugins
-context = base_context
-for p in plugins:
-    context.update(p.get_context())
+    # Collect contexts from all plugins
+    context = base_context
+    for p in plugins:
+        context.update(p.get_context())
 
-jinja_env = Environment(
-    loader=FileSystemLoader("templates"),
-    autoescape=select_autoescape(),
-    trim_blocks=True,
-    lstrip_blocks=True,
-)
+    return context
 
-try:
-    template_name = sys.argv[1]
-except IndexError:
-    template_name = "main.rml"
+def load_main_template():
+    jinja_env = Environment(
+        loader=FileSystemLoader("templates"),
+        autoescape=select_autoescape(),
+        trim_blocks=True,
+        lstrip_blocks=True,
+    )
 
-template = jinja_env.get_template(template_name)
+    template = jinja_env.get_template(config.RAFTHERCAL_MAIN_TEMPLATE)
+    return template
 
+def main():
+    context = load_context()
+    template = load_main_template()
+    out_str = template.render(context)
+    print_from_str(out_str, config.RAFTHERCAL_SERIAL_DEVICE or sys.stdout)
 
-out_str = template.render(context)
-
-print_from_str(out_str, sys.stdout)
+if __name__ == '__main__':
+    main()
