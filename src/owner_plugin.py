76ce78238fe816049918a8886eebe2e0d00bb09c
Raffael Mancini <raffael.mancini@mnhn.lu>
2025-03-04 08:47:27 +0100

    Added birthday to owner plugin

--- src/owner_plugin.py
+++ src/owner_plugin.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from plugin import BasePlugin
 
 class OwnerPlugin(BasePlugin):
@@ -5,4 +7,5 @@ class OwnerPlugin(BasePlugin):
         c = self.get_config()
         return {
             'owner_name': c.OWNER_NAME,
+            'owner_birthday': date.fromisoformat(c.OWNER_BIRTHDAY) if c.OWNER_BIRTHDAY else None,
         }
