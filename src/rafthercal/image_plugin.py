aba29ee486165043bc2165c33dc1c3739238171d
Raffael Mancini <raffael.mancini@mnhn.lu>
2025-03-06 22:58:38 +0100

    Support image resizing

--- src/rafthercal/image_plugin.py
+++ src/rafthercal/image_plugin.py
@@ -14,7 +14,13 @@ class ImagePlugin(BasePlugin):
         r = getattr(c, "IMAGE_CROP_RIGHT", img.size[0])
         b = getattr(c, "IMAGE_CROP_BOTTOM", img.size[1])
         cropped_img = img.crop((l, t, r, b))
-        cropped_img.save(c.IMAGE_OUTPUT_PATH)
+
+        new_width = getattr(c, "IMAGE_RESIZE_WIDTH", 200)
+        new_height = int(cropped_img.height*(new_width/cropped_img.width))
+        resized_img = cropped_img.resize((new_width, new_height),
+                                         resample=Image.Resampling.BICUBIC)
+
+        resized_img.save(c.IMAGE_OUTPUT_PATH)
 
         return {
             'image': "{ image " + c.IMAGE_OUTPUT_PATH + "}",
