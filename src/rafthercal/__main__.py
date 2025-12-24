e5e2a61735643b12794f8bad572b4c217a0fce69
Raffael Mancini <raffael.mancini@mnhn.lu>
2025-12-24 15:49:19 +0100

    Added main module so you can run rafthercal using python3 -m rafthercal

---
+++ src/rafthercal/__main__.py
@@ -0,0 +1,7 @@
+from rafthercal.rafthercal import button_loop
+
+print("========================================")
+print("==== Raf's thermal printer calendar ====")
+print("========================================")
+
+button_loop()
