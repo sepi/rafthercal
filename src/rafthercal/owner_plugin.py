100ff8157796046a49877edaba9b497f817b22b3
Raffael Mancini <raffael.mancini@mnhn.lu>
2025-12-25 16:50:48 +0100

    Congratulate on birthday

--- src/rafthercal/owner_plugin.py
+++ src/rafthercal/owner_plugin.py
@@ -5,7 +5,14 @@ from rafthercal.plugin import BasePlugin
 class OwnerPlugin(BasePlugin):
     def get_context(self):
         c = self.get_config()
+        birthday = date.fromisoformat(c.OWNER_BIRTHDAY) if c.OWNER_BIRTHDAY else None
+        birthday_today = False
+        if birthday and \
+           birthday.month == date.today().month and \
+           birthday.day == date.today().day:
+            birthday_today = True
         return {
             'owner_name': c.OWNER_NAME,
-            'owner_birthday': date.fromisoformat(c.OWNER_BIRTHDAY) if c.OWNER_BIRTHDAY else None,
+            'owner_birthday': birthday,
+            'owner_birthday_today': birthday_today,
         }
